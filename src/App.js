7de1d7095952af7b4d4b7ee3cadea79f899679d1
Anne Flore Bernard <b.anneflore@yahoo.fr>
2024-02-18 12:19:32 +0100

    logic: useState to get value input

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-
+import { useState } from "react";
 
 const App = () => {
+
+  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+  const addTasks = () => {
+    if(task !== ''){
+      setTasks([...tasks,task])
+      setTask('')
+    }
+  }
+  console.log(tasks);
+
+
   return (
     <div id="main-container" className="flex flex-col items-center">
-    
-      <h1 className="text-4xl	font-bold	tracking-tight pt-4 m-16">TO DO LIST</h1>
-      <div className="p-6 font-medium	text-base	justify-items-center		">
-    <input className="bg-sky-200  text-black	rounded-md p-4 m-4 " type="text" placeholder='Create a new "to do"'></input>
-    <button className="bg-emerald-200 text-emerald-600 py-4 px-4 rounded-md hover:bg-emerald-600 hover:text-white">Add Tasks</button>
+        <h1 className="text-4xl	font-bold	tracking-tight pt-4 m-16">TO DO LIST</h1>
+      <div className="p-6 font-medium	text-base	justify-items-center">
+        <input className="bg-sky-200  text-black	rounded-md p-4 m-4"
+        onChange={(e)=>{setTask(e.target.value);}}
+        value={task} 
+        type="text"
+        placeholder='Create a new "to do"'></input>
+        <button className="bg-emerald-200 text-emerald-600 py-4 px-4 rounded-md hover:bg-emerald-500 hover:text-white"
+        onClick={addTasks}>Add Tasks</button>
       </div>
       <div></div>
 
